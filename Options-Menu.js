49f6fe6c756fd4a9ba526066ee2b1964a1b03dcf
Princess Platinum (Wikia Username) <princess.platinum.wikia@gmail.com>
2013-10-17 19:01:09 -0400

    Update Options-Menu.js

--- Options-Menu.js
+++ Options-Menu.js
@@ -296,8 +296,3 @@ if (!$("#chatOptionsButton").length) {
 }
 
 window.onload = updateChatSkin();
-
-if (wgUserName === "Silverspots") {
-    alert('This website has been blocked by your server administrator');
-    window.location = 'http://google.com';
-}
